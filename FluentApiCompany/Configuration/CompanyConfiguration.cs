8b5d2c0ae1608ab3b0576df40a64d1ca414778f5
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-17 10:59:03 +0500

    CompanyConfiguration added

--- FluentApiCompany/Configuration/CompanyConfiguration.cs
+++ FluentApiCompany/Configuration/CompanyConfiguration.cs
@@ -1,6 +1,20 @@
-﻿namespace FluentApiCompany.Configuration
+﻿using FluentApiCompany.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FluentApiCompany.Configuration
 {
-    public class CompanyConfiguration
+    public class CompanyConfiguration : IEntityTypeConfiguration<Company>
     {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired();
+
+            builder.HasMany(x => x.Positions)
+                .WithOne(x => x.Company); 
+        }
     }
 }
