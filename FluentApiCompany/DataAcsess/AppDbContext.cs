7ee17b3fc700fa542c1cb8f3c4719af32e2a9053
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-17 10:33:11 +0500

    data access

--- FluentApiCompany/DataAcsess/AppDbContext.cs
+++ FluentApiCompany/DataAcsess/AppDbContext.cs
@@ -1,6 +1,17 @@
-﻿namespace FluentApiCompany.DataAcsess
+﻿using FluentApiCompany.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FluentApiCompany.DataAcsess
 {
-    public class AppDbContext
+    public class AppDbContext:DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
+        
+        public virtual DbSet<Company> Company { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<Position> Positions { get; set; }
+            
+        
+
     }
 }
