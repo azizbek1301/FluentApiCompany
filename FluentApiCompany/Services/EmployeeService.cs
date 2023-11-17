367d7b4a22665881067d023b23d9779cf791c455
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-17 10:44:55 +0500

    Employee Service Created

--- FluentApiCompany/Services/EmployeeService.cs
+++ FluentApiCompany/Services/EmployeeService.cs
@@ -1,6 +1,110 @@
-﻿namespace FluentApiCompany.Services
+﻿using FluentApiCompany.DataAcsess;
+using FluentApiCompany.DTOs;
+using FluentApiCompany.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FluentApiCompany.Services
 {
-    public class EmployeeService
+    public class EmployeeService : IEmployeeService
     {
+        private readonly AppDbContext _context;
+        public EmployeeService(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+            
+        }
+        public async ValueTask<bool> CreateEmployeeAsync(EmployeDto employeDto)
+        {
+            try
+            {
+                var res = new Employee()
+                {
+                    Name = employeDto.Name,
+                    Surname = employeDto.Surname,
+                    PositionId = employeDto.PositionId,
+                };
+                await _context.Employees.AddAsync(res);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+
+        public async ValueTask<string> DeleteAsync(int id)
+        {
+            try
+            {
+                var res = await _context.Employees.FirstOrDefaultAsync(x => x.Id == id);
+                if(res !=null)
+                {
+                    _context.Employees.Remove(res);
+                    await _context.SaveChangesAsync();
+                    return "Deleted";
+                }
+                else
+                {
+                    return "Not Found";
+                }
+
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+
+        public async ValueTask<List<Employee>> GetAllAsync()
+        {
+            try
+            {
+                var res = await _context.Employees.AsNoTracking().ToListAsync();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async ValueTask<Employee> GetByIdAsync(int id)
+        {
+            try
+            {
+                var res = await _context.Employees.FirstOrDefaultAsync(x => x.Id == id);
+                return res;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async ValueTask<string> UpdateEmployeeAsync(int id, EmployeDto employeeDto)
+        {
+            try
+            {
+                var emp = await _context.Employees.FirstOrDefaultAsync(x => x.Id == id);
+                if (emp != null)
+                {
+                    emp.PositionId = employeeDto.PositionId;
+                    emp.Name = employeeDto.Name;
+                    emp.Surname = employeeDto.Surname;
+                    await _context.SaveChangesAsync();
+                    return "Employee Updated";
+                }
+                else
+                {
+                    return "Employee not found!";
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
     }
 }
--- FluentApiCompany/Services/IEmployeeService.cs
+++ FluentApiCompany/Services/IEmployeeService.cs
@@ -8,7 +8,7 @@ namespace FluentApiCompany.Services
         public ValueTask<bool> CreateEmployeeAsync(EmployeDto employeDto);
         public ValueTask<List<Employee>> GetAllAsync();
         public ValueTask<Employee> GetByIdAsync(int id);
-        public ValueTask<bool> DeleteAsync(int id);
-        public ValueTask<bool> UpdateEmployeeAsync(int id, EmployeDto employeeDto);
+        public ValueTask<string> DeleteAsync(int id);
+        public ValueTask<string> UpdateEmployeeAsync(int id, EmployeDto employeeDto);
     }
 }
