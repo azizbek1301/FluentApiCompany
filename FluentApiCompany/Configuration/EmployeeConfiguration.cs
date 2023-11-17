bbdda518009e2439367f6a154321516a352c511f
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-17 10:59:16 +0500

    EmployeeConfiguration created

--- FluentApiCompany/Configuration/EmployeeConfiguration.cs
+++ FluentApiCompany/Configuration/EmployeeConfiguration.cs
@@ -1,6 +1,25 @@
-﻿namespace FluentApiCompany.Configuration
+﻿using FluentApiCompany.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FluentApiCompany.Configuration
 {
-    public class EmployeeConfiguration
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
     {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .HasMaxLength(30)
+                .IsRequired();
+            
+            builder.Property(x=>x.Surname)
+                .HasMaxLength (30)
+                .IsRequired();
+
+            builder.HasOne(x => x.Position)
+                .WithMany(x => x.Employees);
+        }
     }
 }
