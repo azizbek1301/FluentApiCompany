e5aaa4104a88066e70192b930fc2f3fb577e2ff4
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-17 09:39:13 +0500

    Employee model created

--- FluentApiCompany/Models/Employee.cs
+++ FluentApiCompany/Models/Employee.cs
@@ -2,5 +2,11 @@
 {
     public class Employee
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int PositionId { get; set; }
+
+        public Position Position { get; set; }
     }
 }
