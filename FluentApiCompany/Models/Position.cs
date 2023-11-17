6959d8a285596616d3021737ac106785c417d127
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-17 09:39:22 +0500

    Popsition model created

--- FluentApiCompany/Models/Position.cs
+++ FluentApiCompany/Models/Position.cs
@@ -2,5 +2,10 @@
 {
     public class Position
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Employee> Employees { get; set; }
+        public int CompanyId {  get; set; }
+        public Company Company { get; set; }
     }
 }
