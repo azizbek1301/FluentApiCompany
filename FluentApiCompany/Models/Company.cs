e26c29541c1ce2fa5ae017c1724776d5be2d4ecc
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-17 09:39:01 +0500

    Company model created

--- FluentApiCompany/Models/Company.cs
+++ FluentApiCompany/Models/Company.cs
@@ -2,5 +2,9 @@
 {
     public class Company
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Position> Positions { get; set; }
     }
 }
