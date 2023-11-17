2c291d3d23edb00a134706866b4a88dd715e2baa
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-17 10:59:36 +0500

    Position configuration ceated

--- FluentApiCompany/Configuration/PositionConfiguration.cs
+++ FluentApiCompany/Configuration/PositionConfiguration.cs
@@ -1,6 +1,24 @@
-﻿namespace FluentApiCompany.Configuration
+﻿using FluentApiCompany.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FluentApiCompany.Configuration
 {
-    public class PositionConfiguration
+    public class PositionConfiguration : IEntityTypeConfiguration<Position>
     {
+        public void Configure(EntityTypeBuilder<Position> builder)
+        {
+            builder.HasKey(x => x.Id);  
+
+            builder.Property(x=>x.Name)
+                .HasMaxLength(30)
+                .IsRequired();
+
+            builder.HasOne(x => x.Company)
+                .WithMany(x => x.Positions);
+
+            builder.HasMany(x => x.Employees)
+                .WithOne(x => x.Position);
+        }
     }
 }
